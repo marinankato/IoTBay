98498844a31620c4ec321265bf6ae670a2248ade
Marina Kato <marina.kato@student.uts.edu.au>
2025-04-09 10:56:05 +1000

    added database and admin user

---
+++ database/queries/createTables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Users (
+    UserID numeric(8),
+    FirstName varchar(50),
+    LastName varchar(50),
+    PhoneNumber varchar(10),
+    Email varchar(150),
+    Password varchar(50),
+    Role varchar(50)
+);
+
+-- To delete an unwanted table:
+-- DROP TABLE User;
+
+-- To delete all rows from table, 
+-- or specific row:
+
+-- DELETE FROM User;
+-- WHERE UserID=1;
---
+++ database/queries/insertEntries.sql
@@ -0,0 +1,3 @@
+INSERT INTO Users
+VALUES 
+(12345678, 'John', 'Doe','0411222333','admin@gmail.com','password123', 'admin' );
