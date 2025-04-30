e8dc7ae8be851cc8725b43cee1272792c1cd473b
Marina <marina.kato@student.uts.edu.au>
2025-04-30 11:11:29 +1000

    note in DB.java

--- src/main/java/model/dao/DB.java
+++ src/main/java/model/dao/DB.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 /* Super class of DAO classes that stores the database information */
 public abstract class DB {
 
+    // this path URL will differ for everyone, to change to complete path
     protected String URL = "jdbc:sqlite:database/iotdb.db";
     protected String driver = "org.sqlite.JDBC"; 
     protected Connection conn; 
