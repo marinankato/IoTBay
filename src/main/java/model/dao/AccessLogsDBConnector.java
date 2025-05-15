a9b4db2e08ead84b721d514fff0a6750aa522e59
Marina <marina.kato@student.uts.edu.au>
2025-05-15 21:01:05 +1000

    note

--- src/main/java/model/dao/AccessLogsDBConnector.java
+++ src/main/java/model/dao/AccessLogsDBConnector.java
@@ -4,6 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// package model.dao;
+// import java.sql.Connection;
+
+// public class AccessLogsDB {
+    
+//     // this path URL will differ for everyone, change to your path to db
+//     protected String URL = "jdbc:sqlite:/Users/marinakato/Intro to Software Dev/IoTBay-1/database/iotdb.db";
+//     protected String driver = "org.sqlite.JDBC";
+//     protected Connection conn;
+
+// }
+// add above to a new file called AccessLogsDB.java with your path URL to DB
+
 public class AccessLogsDBConnector extends AccessLogsDB {
     public AccessLogsDBConnector() throws ClassNotFoundException, SQLException {
         Class.forName(driver);
